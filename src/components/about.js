0d67806a73b25e3f5875e0e98ec3bb4fcaa476cf
cynthia <cynthiakagz@gmail.com>
2023-10-10 11:30:04 +0300

    about us

--- src/components/about.js
+++ src/components/about.js
@@ -1,25 +1,25 @@
 import React from "react";
 import "./styles/style.css";
 
-const AboutUs = () => {
+const AboutUS = () => {
   return (
     <div className="meet-the-team">
       <div className="overlap-group">
         <div className="overlap">
           <img className="rectangle" alt="Rectangle" src="rectangle-89.svg" />
-          <img className="image" alt="Image" src="image-16.png" />
+          <img className="image" alt="Image" src="https://t3.ftcdn.net/jpg/01/40/16/38/360_F_140163883_oivRepH5pIw0jXDKrd50iB9334qiQXt4.jpg" />
         </div>
         <div className="div">
           <img className="rectangle" alt="Rectangle" src="rectangle-90.svg" />
-          <img className="img" alt="Image" src="image-15.png" />
+          <img className="img" alt="Image" src="https://img.freepik.com/premium-photo/young-african-american-man-stands-with-crossed-arms-dark-gym-smiles-fitness-trainer_164411-4316.jpg" />
         </div>
         <div className="overlap-2">
           <img className="rectangle" alt="Rectangle" src="rectangle-91.svg" />
-          <img className="image-2" alt="Image" src="image-12.png" />
+          <img className="image-2" alt="Image" src="https://img.freepik.com/free-photo/handsome-black-man-is-engaged-gym_1157-29625.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais" />
         </div>
         <div className="overlap-3">
           <img className="rectangle-2" alt="Rectangle" src="rectangle-92.svg" />
-          <img className="image-3" alt="Image" src="image-11.png" />
+          <img className="image-3" alt="Image" src="https://img.freepik.com/premium-photo/athlete-standing-gym-preparing-training_873069-8925.jpg" />
         </div>
         <div className="overlap-4">
           <div className="text-wrapper">MEET THE TEAM</div>
@@ -36,11 +36,6 @@ const AboutUs = () => {
             <div className="group">
               <img className="bx-bx-search-alt" alt="Bx bx search alt" src="bx-bx-search-alt-2.svg" />
             </div>
-            <div className="text-wrapper-3">HOME</div>
-            <div className="text-wrapper-4">CLASSES</div>
-            <div className="text-wrapper-5">LOG IN</div>
-            <div className="text-wrapper-6">ABOUT US</div>
-            <div className="text-wrapper-7">CONTACT US</div>
           </div>
         </div>
         <div className="button-wrapper">
@@ -70,4 +65,4 @@ const AboutUs = () => {
     </div>
   );
 };
-export default AboutUs;
+export default AboutUS;
