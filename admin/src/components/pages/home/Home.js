52ed718d00f4a0508bb7ce919003fc9e1cb6b757
Future <futureworldzone@gmail.com>
2023-10-13 16:40:50 +0300

    Added inline css to new variables and the Menu label

--- admin/src/components/pages/home/Home.js
+++ admin/src/components/pages/home/Home.js
@@ -6,17 +6,20 @@ import { Checkbox, Grid, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
 export const Home = () => {
 
+    const panelcss = "flex flex-col min-h-screen items-center justify-center dark:bg-gray-800 dark:text-white";
+    const labelcss = "items-center justify-center dark:bg-gray-800 dark:text-white mb-6";
+    const buttoncss = "text-lg bg-gray-200 dark:bg-gray-700 rounded-xl px-8 py-4"
     const [visible, setVisible] = useState(false)
 
     return (
 
-        <Grid columns={1} className={"flex flex-col min-h-screen items-center justify-center dark:bg-gray-800 dark:text-white"}>
+        <Grid columns={1} className={panelcss}>
 
         <Grid.Column>
             <Grid.Row>
                 <Checkbox icon
                     checked={visible}
-                    label={{ children: <Icon name='sidebar' />}}
+                    label={{ children: <div className={labelcss}> Menu <Icon name='sidebar' /> </div> }}
                     onChange={(e, data) => setVisible(data.checked)}
                 />
                 <ThemeToggle />
@@ -67,12 +70,12 @@ export const Home = () => {
           <Sidebar.Pusher dimmed={visible}>
             <Segment basic>
 
-        <div className={"flex flex-col min-h-screen items-center justify-center dark:bg-gray-800 dark:text-white"}>
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="text-2xl mb-6">The Administrator Panel</h1>
-            <a href={'http://localhost:3000/'} target={'_blank'} rel={'noreferrer'} className={'text-lg bg-gray-200 dark:bg-gray-700 rounded-xl px-8 py-4'}>Go to client homepage</a>
-            <ThemeToggle />
-        </div>
+              <div className={panelcss}>
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h1 className="text-2xl mb-6">The Administrator Panel</h1>
+                  <a href={'http://localhost:3000/'} target={'_blank'} rel={'noreferrer'} className={buttoncss}>Go to client homepage</a>
+                  <ThemeToggle />
+              </div>
             
             </Segment>
           </Sidebar.Pusher>
