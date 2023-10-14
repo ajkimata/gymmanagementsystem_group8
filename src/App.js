9c6924a63a86bb64826a6f42873c86e6d2a6bafc
alfred kimata <alfyjason@gmail.com>
2023-10-14 18:59:24 +0300

    completed admin template

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import HomeAdmin from "./admin/HomeAdmin";
 import { Classes } from "./components/classes";
 import Login from "./components/login";
 import AboutUs from "./components/about";
 import { Contact } from "./components/contact";
 import { Register } from "./components/Register";
+import Header from "./admin/Header";
+import SideNav from "./admin/SideNav";
+import Footer from "./admin/Footer";
+import "./App.css";
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        {/* Client-side routes */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/classes" element={<Classes />} />
@@ -19,6 +25,21 @@ function App() {
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/Register" element={<Register />} />
+        </Routes>
+
+        {/* Admin-side routes */}
+        <Routes>
+          <Route
+            path="/admin"
+            element={
+              <div className="wrapper">
+                <Header />
+                <Footer />
+                <HomeAdmin />
+                <SideNav />
+              </div>
+            }
+          />
         </Routes>
       </div>
     </Router>
