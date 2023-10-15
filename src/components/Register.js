c745736b30e6c19430a5da5f8310f525bb2ce2e4
cynthia <cynthiakagz@gmail.com>
2023-10-15 10:55:03 +0300

    register

--- src/components/Register.js
+++ src/components/Register.js
@@ -97,16 +97,16 @@ export const Register = () => {
               <div className="group">
                 <div className="text-wrapper-2">Username</div>
               </div>
-              <img className="bx-user" alt="Bx user" src="bx-user.svg" />
+              <img className="bx-user" alt="Bx user" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSpAnV7Eqy1ajdV__BGRrAU9KkCa3aJGDly4funAkQzosa99R5KIOe4oFG4e_H3cNfgFw&usqp=CAU" />
             </div>
             <div className="email">
               <div className="group-2">
                 <input className="input" placeholder="Email" type="email" />
               </div>
-              <img className="mdi-light-email" alt="Mdi light email" src="mdi-light-email.svg" />
+              <img className="mdi-light-email" alt="Mdi light email" src="https://png.pngtree.com/png-vector/20190927/ourmid/pngtree-email-icon-png-image_1757854.jpg" />
             </div>
             <div className="password">
-              <img className="iconamoon-eye-light" alt="Iconamoon eye light" src="iconamoon-eye-light.svg" />
+              {/* <img className="iconamoon-eye-light" alt="Iconamoon eye light" src="iconamoon-eye-light.svg" /> */}
               <img className="circum-lock" alt="Circum lock" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIHZ-yIurzqBWnkQMLgjbFUZpO2pLfE42hC_poX74&s" />
               <div className="text-wrapper-3">Password</div>
             </div>
@@ -129,7 +129,7 @@ export const Register = () => {
       </div>
       <div className="glogin">
         <div className="frame-2">
-          <img className="google" alt="Google" src="google-1.png" />
+          <img className="google" alt="Google" src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" />
           <div className="text-wrapper-7">Continue With Google</div>
         </div>
       </div>
@@ -137,7 +137,7 @@ export const Register = () => {
         <p className="p">Please Sign Up for an awesome experience</p>
       </div>
       <div className="text-wrapper-8">WELCOME TO POWERHOUSE GYM</div>
-      <img className="image" alt="Image" src="image.png" />
+      <img className="image" alt="Image" src="" />
       <p className="powerhouse">
         <span className="text-wrapper-9">P</span>
         <span className="text-wrapper-10">owerhouse</span>
