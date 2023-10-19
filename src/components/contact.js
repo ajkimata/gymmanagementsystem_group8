2c8e971b97a2632531872cf4e36aa82d1bc144e2
Moffat <moffat.muga@gmail.com>
2023-10-19 19:11:26 +0300

    initial contact

---
+++ src/components/contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './contact.css';
+
+function Contact() {
+  return (
+    <div className="contact-page">
+      <div className="top-image">
+        <img src='image 6.png' alt='image-1'/>
+      </div>
+      <div className="bottom-image">
+      <img src='image20.png' alt='image-2'/>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
