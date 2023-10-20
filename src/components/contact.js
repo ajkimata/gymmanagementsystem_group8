7a55de56728db86ed8fd1cae5aab21468125a25f
Moffat <moffat.muga@gmail.com>
2023-10-20 17:49:29 +0300

    added subscribe section

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,6 +6,12 @@ function Contact() {
     <div className="contact-page">
       <div className="top-image">
         <img src='image 6.png' alt='image-1'/>
+        <p className="stay-updated">Stay up to date with our story</p>
+        <div className="email-form">
+          <p className="email-label">Enter email address:</p>
+          <input type="email" className="email-input" placeholder="Enter your email"/>
+          <button className="join-button">Join Now</button>
+        </div>
       </div>
       <div className="bottom-image">
         <img src='image20.png' alt='image-2'/>
