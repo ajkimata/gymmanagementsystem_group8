06efd01d643a008785ce08fb6e485ae69b3a8d78
Moffat <moffat.muga@gmail.com>
2023-10-21 15:48:46 +0300

    styled classes

--- src/components/classes.js
+++ src/components/classes.js
@@ -15,7 +15,7 @@ function Classes()  {
             <img src="image-4.png" alt="Class photo 1" />
             <span className="beginner-text">BEGINNER</span>
           </div>
-          <p>Explore our beginner-friendly classes perfect for those new to fitness.</p>
+          <p><span className="span">Explore</span> our beginner-friendly classes perfect for those new to fitness.</p>
           <button>Enroll</button>
         </div>
         <div className="class-card">
@@ -23,7 +23,7 @@ function Classes()  {
             <img src="image-3.png" alt="Class photo 2" />
             <span className="beginner-text">INTERMEDIATE</span>
           </div>
-          <p>Achieve your fitness goals by establishing a tailored fitness routine</p>
+          <p><span className="span">Achieve</span> your fitness goals by establishing a tailored fitness routine</p>
           <button>Enroll</button>
         </div>
         <div className="class-card">
@@ -31,7 +31,7 @@ function Classes()  {
             <img src="image-1.png" alt="Class photo 3" />
             <span className="beginner-text">ADVANCED</span>
           </div>
-          <p>Join our advanced gym class designed for seasoned fitness ethusiats</p>
+          <p><span className="span">Join</span> our advanced gym class designed for seasoned fitness ethusiats</p>
           <button>Enroll</button>
         </div>
       </div>
