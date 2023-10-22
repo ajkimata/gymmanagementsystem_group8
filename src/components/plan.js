2936e450598ce5412ea18ce5437e5b8eaaae5675
Moffat <moffat.muga@gmail.com>
2023-10-22 14:24:22 +0300

    created payments

---
+++ src/components/plan.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Plan() {
+  return (
+    <div className='plans-page'>
+        <div className='unlock-plans'>
+            <p>Unlock the Full Potential of Your 
+            <br/>
+            Fitness with Our Membership Plans</p>
+            <span>We offer a range of membership options designed to meet your needs and help you achieve your goals.</span>
+        </div>
+
+        <div className='payment-cards'>
+            <div className='basic-card'>
+                <p> Basic membership</p>
+                <p>Kshs.1000<span>/Month</span></p>
+                <hr/>
+                <p>Includes access to our gym facilities
+                    <br/>
+                    and group fitness classes
+                </p>
+                <button>Join Now</button>
+            </div>
+            <div className='premium-card'>
+                <p> Basic membership</p>
+                <p>Kshs.1000<span>/Month</span></p>
+                <hr/>
+                <p>Includes access to our gym facilities
+                    <br/>
+                    and group fitness classes
+                </p>
+                <button>Join Now</button>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Plan
