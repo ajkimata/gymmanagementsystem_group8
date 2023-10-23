2e8def55c4aa05cef87d6b2ad4e950d83b8147fb
alfred kimata <alfyjason@gmail.com>
2023-10-23 17:19:29 +0300

    updated the teams card

--- src/components/team.js
+++ src/components/team.js
@@ -2,39 +2,38 @@ import React from "react";
 import "./styles/team.css";
 
 const trainers = [
-  { name: "CYNTHIA", imgSrc: "/Assets/advanced.jpg", alt: "trainer-1" },
-  { name: "ALEX", imgSrc: "Assets/advanced.jpg", alt: "trainer-2" },
-  { name: "ALFRED", imgSrc: "Assets/advanced.jpg", alt: "trainer-3" },
-  { name: "MOFFAT", imgSrc: "Assets/advanced.jpg", alt: "trainer-4" },
+  { name: "CYNTHIA", imgSrc: "/Assets/Login.jpeg", alt: "Cynthia" },
+  { name: "ALEX", imgSrc: "/Assets/beginner.jpg", alt: "Alex" },
+  { name: "ALFRED", imgSrc: "/Assets/advanced.jpg", alt: "Alfred" },
+  { name: "MOFFAT", imgSrc: "/Assets/Register.jpeg", alt: "Moffat" },
 ];
 
 const Team = () => {
   return (
-    <div className="background-div">
-      <div className="frame-13">
-        <div className="header-container">
-          <span className="meet-the-team-text">MEET THE TEAM</span>
-          <div className="arrow-container">
-            <img src="Vector.png" alt="Arrow" className="arrow-image" />
-            <img src="Vector.png" alt="Arrow" className="arrow-image" />
-            <img src="Vector.png" alt="Arrow" className="arrow-image" />
+    <div className="team-background">
+      <h1 className="team-title">MEET THE TEAM</h1>
+      <div className="trainers-container">
+        {trainers.slice(0, 3).map((trainer, index) => (
+          <div key={index} className="trainer-card">
+            <img
+              className="trainer-image"
+              src={trainer.imgSrc}
+              alt={trainer.alt}
+            />
+            <div className="trainer-name">{trainer.name}</div>
           </div>
+        ))}
+      </div>
+      <div className="trainers-bottom">
+        <div className="trainer-card moffat-card">
+          <img
+            className="trainer-image"
+            src={trainers[3].imgSrc}
+            alt={trainers[3].alt}
+          />
+          <div className="trainer-name">{trainers[3].name}</div>
         </div>
-
-        <div className="meet-the-team">
-          {trainers.map((trainer, index) => (
-            <div key={index} className="rectangle-container">
-              <img
-                className="trainer-image"
-                src={trainer.imgSrc}
-                alt={trainer.alt}
-              />
-              <div className="trainer-button">
-                <div className="trainer-name">{trainer.name}</div>
-              </div>
-            </div>
-          ))}
-        </div>
+        <button className="browse-all-button">Browse All Trainers</button>
       </div>
     </div>
   );
