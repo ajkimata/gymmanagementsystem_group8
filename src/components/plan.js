db1d83ce84fbdd5eb83316fbe5205c7b4ef199af
alfred kimata <alfyjason@gmail.com>
2023-10-23 19:43:05 +0300

    updated the plan

--- src/components/plan.js
+++ src/components/plan.js
@@ -5,43 +5,36 @@ function Plan() {
   return (
     <div className="plans-page">
       <div className="unlock-plans">
-        <p className="gothic-word">
-          Unlock the Full Potential of Your
-          <br />
-          Fitness with Our Membership Plans
-        </p>
-        <span>
+        <h1 className="gothic-word">
+          Unlock the Full Potential of Your Fitness with Our Membership Plans
+        </h1>
+        <p>
           We offer a range of membership options designed to meet your needs and
           help you achieve your goals.
-        </span>
+        </p>
       </div>
 
       <div className="payment-card-container">
         <div className="basic-card">
-          <p className="gothic-word"> Basic membership</p>
-          <p className="gothic-word">Kshs.1000</p>
+          <h2 className="gothic-word">Basic membership</h2>
+          <p className="price gothic-word">Kshs.1000</p>
           <span>/Month</span>
           <hr />
-          <p>
-            Includes access to our gym facilities
-            <br />
-            and group fitness classes
-          </p>
-          <button>Join Now</button>
+          <p>Includes access to our gym facilities and group fitness classes</p>
+          <button className="basic-button">Join Now</button>
         </div>
+
         <div className="premium-card">
-          <p className="gothic-word"> Premium membership</p>
-          <p className="gothic-word">Kshs.5000</p>
+          <h2 className="gothic-word">Premium membership</h2>
+          <p className="price gothic-word">Kshs.5000</p>
           <span>/Month</span>
           <hr />
           <p>
             Includes all the benefits of our Basic Membership, plus unlimited
-            access to our premium
-            <br />
-            amenities, such as sauna and steam rooms, personal training
-            sessions, and more.
+            access to our premium amenities, such as sauna and steam rooms,
+            personal training sessions, and more.
           </p>
-          <button>Join Now</button>
+          <button className="premium-button">Join Now</button>
         </div>
       </div>
     </div>
